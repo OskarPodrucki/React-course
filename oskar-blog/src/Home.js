ac636f0e66adaad198ec154707e1a9162f594150
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-12-17 18:54:29 +0100

    ...

--- oskar-blog/src/Home.js
+++ oskar-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handeClick = () => {
+        console.log('hello, user');
+    }
+
+    const handeClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target)
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handeClick}>Click me</button>
+            <button onClick={(e) => handeClickAgain('oskar', e)}>Click me again</button>
         </div>
      );
 }

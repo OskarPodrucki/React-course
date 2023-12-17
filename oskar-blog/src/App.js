abf46a60e9367b676227d916282b5464d451b4ae
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-12-17 18:25:00 +0100

    ...

--- oskar-blog/src/App.js
+++ oskar-blog/src/App.js
@@ -2,10 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog!'
+  const like = 50;
+  // const person = { name: 'oskar', age: 30};
+  const link = "https://github.com/OskarPodrucki";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>First React application!</h1>
+        <h1>{ title }</h1>
+        <p>Liked { like } times</p>
+        
+        {/* <p> { person } </p> */}
+        
+        <p>{ 10 }</p>
+        <p>{ 'hey, everyone' }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link } target='_blank'>My github</a>
       </div>
     </div>
   );

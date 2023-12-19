2d950dbf7b45584f14f38ba45e597a5951868d66
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-12-19 19:00:22 +0100

    ...

--- oskar-blog/src/App.js
+++ oskar-blog/src/App.js
@@ -1,15 +1,15 @@
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  return (
-      <div className="App">
-        <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			<div className='content'>
+				<Home />
+			</div>
+		</div>
+	);
 }
 
 export default App;
